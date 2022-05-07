c74bca195d790bdb61216cfce714279794d7fb0f
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-07 23:13:48 +0900

    testのメッセージを追加した

--- renderer/pages/__tests__/index.spec.tsx
+++ renderer/pages/__tests__/index.spec.tsx
@@ -2,7 +2,7 @@ import IndexPage from "../index";
 import { render } from "@testing-library/react";
 
 describe("IndexPageコンポーネント", () => {
-  test("", () => {
+  test("「再生」ボタンが作成された", () => {
     const { getByText } = render(<IndexPage />);
     expect(getByText("再生")).toBeTruthy();
   });
