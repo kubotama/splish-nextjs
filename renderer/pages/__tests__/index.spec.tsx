3a4f598646c39fc9fa90433165f119d4cfc7c60c
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-13 21:11:13 +0900

    テスト用のクリックイベントを修正した
    fireEvent -> userEvent
    async/awaitを追加

--- renderer/pages/__tests__/index.spec.tsx
+++ renderer/pages/__tests__/index.spec.tsx
@@ -1,5 +1,6 @@
 import IndexPage from "../index";
 import { render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("IndexPageコンポーネント", () => {
   test("「再生」ボタンが作成された", () => {
@@ -7,14 +8,14 @@ describe("IndexPageコンポーネント", () => {
     expect(getByText("再生")).toBeTruthy();
   });
 
-  test("「再生」ボタンがクリックされた", () => {
+  test("「再生」ボタンがクリックされた", async () => {
     const { container, getByText } = render(<IndexPage />);
     const mockClickButton = (container.onclick = jest.fn());
     const button = getByText("再生");
 
     expect(mockClickButton).toHaveBeenCalledTimes(0);
 
-    fireEvent.click(button);
+    await userEvent.click(button);
 
     expect(mockClickButton).toHaveBeenCalledTimes(1);
   });
