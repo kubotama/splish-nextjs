91a9eba6758b7ecbe8f8b9dac3a04056829f2a88
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-20 21:57:20 +0900

    登録ボタンを作成した

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,26 +1,12 @@
-import React, { useState } from "react";
-
-import { Howl, Howler } from "howler";
+import React from "react";
 
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
-  const [labelPlayButton, setLabelPlayButton] = useState("再生");
-  const toggleLabelPlayButton = () => {
-    setLabelPlayButton(labelPlayButton === "再生" ? "停止" : "再生");
-  };
-  const onClickPlay = () => {
-    const howler = new Howl({
-      src: ["/1.wav"],
-    });
-    howler.play();
-    toggleLabelPlayButton();
-  };
-
   return (
     <Layout title="Home | splish (speak and listen, shadow)">
       <div>
-        <button onClick={onClickPlay}>{labelPlayButton}</button>
+        <button>登録</button>
       </div>
     </Layout>
   );
