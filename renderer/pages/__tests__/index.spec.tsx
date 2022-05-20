cbf4e0a2de32ab073ce715d31bd31d767f8c8534
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-20 22:22:16 +0900

    起動時に表示される要素を作成した

--- renderer/pages/__tests__/index.spec.tsx
+++ renderer/pages/__tests__/index.spec.tsx
@@ -2,8 +2,18 @@ import IndexPage from "../index";
 import { render } from "@testing-library/react";
 
 describe("起動時の画面", () => {
-  it("登録ボタンが表示される", () => {
-    const { getByText } = render(<IndexPage />);
+  it("起動時の画面をテストする", () => {
+    const { getByText, getByPlaceholderText, getByTestId } = render(
+      <IndexPage />
+    );
+    expect(getByText("登録するテキスト")).toBeInTheDocument();
+    expect(getByPlaceholderText("登録するテキストを入力")).toBeInTheDocument();
     expect(getByText("登録")).toBeInTheDocument();
+
+    expect(getByText("登録されたテキスト")).toBeInTheDocument();
+    expect(getByTestId("registeredText")).toHaveTextContent("");
+    expect(getByText("変換")).toBeInTheDocument();
+
+    expect(getByText("再生")).toBeInTheDocument();
   });
 });
