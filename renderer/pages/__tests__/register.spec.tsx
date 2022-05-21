3b7de51438d607d807c3e64ca86fa105f62086e0
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-21 18:20:18 +0900

    不要なimportを削除した

--- renderer/pages/__tests__/register.spec.tsx
+++ renderer/pages/__tests__/register.spec.tsx
@@ -1,52 +1,47 @@
-import { render, screen, renderHook } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import IndexPage from "../index";
 
-import { useRegisterBlock } from "../../hooks/UseRegisterBlock";
-
 describe("登録ブロック", () => {
-  it("登録するテキストの入力欄(inputText)が空文字でない場合、登録ボタン(registerButton)が有効化されることをテストする。", async () => {
-    const { getByPlaceholderText, getByText } = render(<IndexPage />);
+  beforeEach(() => {
+    render(<IndexPage />);
+  });
 
-    expect(getByText("登録")).toBeDisabled();
+  it("登録するテキストの入力欄(inputText)が空文字でない場合、登録ボタン(registerButton)が有効化されることをテストする。", async () => {
+    expect(screen.getByText("登録")).toBeDisabled();
 
     await userEvent.type(
-      getByPlaceholderText("登録するテキストを入力"),
+      screen.getByPlaceholderText("登録するテキストを入力"),
       "テスト"
     );
 
-    expect(getByText("登録")).toBeEnabled();
+    expect(screen.getByText("登録")).toBeEnabled();
   });
 
   it("登録するテキストの入力欄(inputText)が空文字になったら、登録ボタン(registerButton)が無効化されることをテストする。", async () => {
-    const { getByPlaceholderText, getByText } = render(<IndexPage />);
-
     await userEvent.type(
-      getByPlaceholderText("登録するテキストを入力"),
+      screen.getByPlaceholderText("登録するテキストを入力"),
       "テスト"
     );
 
-    expect(getByText("登録")).toBeEnabled();
+    expect(screen.getByText("登録")).toBeEnabled();
 
-    await userEvent.clear(getByPlaceholderText("登録するテキストを入力"));
+    await userEvent.clear(
+      screen.getByPlaceholderText("登録するテキストを入力")
+    );
 
-    expect(getByText("登録")).toBeDisabled();
+    expect(screen.getByText("登録")).toBeDisabled();
   });
 
   it("登録ボタン(registerButton)がクリックされたら、登録されたテキスト(registeredText)に、登録するテキストの入力欄(inputText)に入力されていた文字列が設定されることをテストする。", async () => {
-    const { getByPlaceholderText, getByText, getByTestId } = render(
-      <IndexPage />
-    );
-
     await userEvent.type(
-      getByPlaceholderText("登録するテキストを入力"),
+      screen.getByPlaceholderText("登録するテキストを入力"),
       "テスト"
     );
 
-    await userEvent.click(getByText("登録"));
+    await userEvent.click(screen.getByText("登録"));
 
-    expect(getByTestId("registeredText")).toHaveTextContent("テスト");
+    expect(screen.getByTestId("registeredText")).toHaveTextContent("テスト");
   });
 });
