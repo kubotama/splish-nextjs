d977bc3c441d27d09aacbe721696217f63035c41
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-21 12:34:02 +0900

    登録ブロックのテストを定義した

---
+++ renderer/pages/__tests__/register.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import RegisterBlock from "../../components/RegisterBlock";
+
+describe("登録ブロック", () => {
+  it.todo(
+    "登録するテキストの入力欄(inputText)が空文字でない場合、登録ボタン(registerButton)が有効化されることをテストする。"
+  );
+  it.todo(
+    "登録するテキストの入力欄(inputText)が空文字になったら、登録ボタン(registerButton)が無効化されることをテストする。"
+  );
+  it.todo(
+    "登録ボタン(registerButton)がクリックされたら、登録されたテキスト(registeredText)に、登録するテキストの入力欄(inputText)に入力されていた文字列が設定されることをテストする。"
+  );
+});
