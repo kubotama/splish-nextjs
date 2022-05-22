7b8cd51fdf7b47c211580b3769c900787f72dd73
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-22 12:59:58 +0900

    登録ボタンがクリックされて登録されたテキストに設定されると、変換ボタンが有効になるテストを追加した

--- renderer/pages/__tests__/tts.spec.tsx
+++ renderer/pages/__tests__/tts.spec.tsx
@@ -1,9 +1,31 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import IndexPage from "../index";
 
 describe("変換ブロック", () => {
-  it.todo(
-    "登録ボタンがクリックされて登録されたテキストに設定されると、変換ボタンが有効になる"
-  );
+  beforeEach(() => {
+    render(<IndexPage />);
+  });
+
+  it("登録ボタンがクリックされて登録されたテキストに設定されると、変換ボタンが有効になる", async () => {
+    const testText = "テスト";
+
+    // 最初は変換ボタンは無効
+    expect(screen.getByText("変換")).toBeDisabled();
+
+    // 入力欄に文字列を入力
+    await userEvent.type(
+      screen.getByPlaceholderText("登録するテキストを入力"),
+      testText
+    );
+
+    // 登録ボタンをクリック
+    await userEvent.click(screen.getByText("登録"));
+
+    // 変換ボタンが有効になる
+    expect(screen.getByText("変換")).toBeEnabled();
+  });
   it.todo(
     "変換ボタンがクリックされると、変換されたテキストに設定されて、再生ボタンが有効になる"
   );
