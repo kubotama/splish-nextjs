07a47ddc5505cbbc74fce90c4cfaf27cb17347d1
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-25 20:38:32 +0900

    テストを成功

--- renderer/hooks/UseSoundBlock.tsx
+++ renderer/hooks/UseSoundBlock.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
+// import { ipcRenderer } from "electron";
+
 import { Howl } from "howler";
 
 export const useSoundBlock = () => {
   const [soundButtonLabel, setSoundButtonLabel] = useState("再生");
-  const onClickSoundButton = () => {
+  const onClickSoundButton = async () => {
     setSoundButtonLabel(soundButtonLabel === "再生" ? "停止" : "再生");
 
+    // ipcRenderer.on("playEnd", (event) => {
+    //   setSoundButtonLabel("再生");
+    // });
+
+    // const filename = "./main.wav";
     const filename = "test.wav";
     const howler = new Howl({
       src: [filename],
@@ -15,6 +22,8 @@ export const useSoundBlock = () => {
       },
     });
     howler.play();
+    // ipcRenderer.send("play", filename);
+    // await (window as any).splish.play(filename);
   };
 
   return { soundButtonLabel, onClickSoundButton };
