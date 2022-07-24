7f6e741fe663780c460bb05bfc7281bc6ff27993
kubotama <16697573+kubotama@users.noreply.github.com>
2022-07-24 20:31:04 +0900

    テキストを音声に変換する機能を追加した

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -4,6 +4,9 @@ import { URL } from "url";
 
 import fs from "fs";
 
+import { TextToSpeechClient } from "@google-cloud/text-to-speech";
+import { google } from "@google-cloud/text-to-speech/build/protos/protos";
+
 // Packages
 import { BrowserWindow, app, ipcMain, Menu } from "electron";
 import isDev from "electron-is-dev";
@@ -34,42 +37,35 @@ app.on("ready", async () => {
   mainWindow.loadURL(url);
   setupWindowMenu(mainWindow);
 
-  ipcMain.handle(IPCKeys.TEST_MESSAGE, () => {
+  ipcMain.handle(IPCKeys.TEST_MESSAGE, async () => {
     const filename = "./output.mp3";
-    try {
-      const buffer = fs.readFileSync(filename);
-      console.log("exist");
+    const client = new TextToSpeechClient();
+    const text = "This is sample speech. This is sample speech.";
+
+    const request: google.cloud.texttospeech.v1.ISynthesizeSpeechRequest = {
+      input: { text: text },
+      voice: {
+        languageCode: "en-US",
+        ssmlGender: "NEUTRAL",
+        name: "en-US-Standard-J",
+      },
+      audioConfig: {
+        audioEncoding: "MP3",
+        effectsProfileId: ["headphone-class-device"],
+      },
+    };
+
+    const [response] = await client.synthesizeSpeech(request);
+    if (response.audioContent) {
+      fs.writeFileSync(filename, response.audioContent);
+      const buffer = response.audioContent;
       return buffer;
-    } catch (error) {
-      console.log("not exist");
-      return;
     }
   });
 });
 
 // Quit the app once all windows are closed
 app.on("window-all-closed", app.quit);
-
-// // listen the channel `message` and resend the received message to the renderer process
-// ipcMain.on("testMessage", () => {
-//   console.log("test message");
-//   return "filename";
-// });
-
-// ipcMain.on("play", (event: IpcMainEvent, filename: string) => {
-//   console.log(__dirname);
-//   console.log(filename);
-//   const howler = new Howl({
-//     src: [filename],
-//     onloaderror: (id: number, err: any) => {
-//       console.log(id + " " + filename + err);
-//     },
-//     onend: () => {
-//       event.sender.send("playEnd", "");
-//     },
-//   });
-//   howler.play();
-// });
 
 const setupWindowMenu = (window: BrowserWindow) => {
   const template = [
