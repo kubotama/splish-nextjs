1b854107c2891fbae2f64cbc7a6efd1c12ee6470
kubotama <16697573+kubotama@users.noreply.github.com>
2022-07-24 15:38:19 +0900

    モジュールを最新版に更新した

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+
+// import ReactPlayer from "react-player";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 import Layout from "../components/Layout";
 // import TtsBlock from "../components/TtsBlock";
@@ -21,6 +25,36 @@ const IndexPage = () => {
     useTtsBlock(registeredText);
 
   const { soundButtonLabel, onClickSoundButton } = useSoundBlock();
+
+  const [file, setFile] = useState("");
+  const [url, setUrl] = useState<string | undefined>(undefined);
+
+  const onClickTest = async () => {
+    window.splish.testMessage().then(async (buffer) => {
+      setFile(buffer.toString());
+      const ctx = new AudioContext();
+      let fakeAudioBuffer = null;
+      const audioBuffer = await ctx.decodeAudioData(buffer.buffer);
+      // fakeAudioBuffer = audioBuffer;
+      const source = ctx.createBufferSource();
+      source.buffer = audioBuffer;
+      // const mediaStreamDest = ctx.createMediaStreamDestination();
+      source.connect(ctx.destination);
+      // const { stream } = mediaStreamDest;
+      // setFile(stream.getAudioTracks().toString());
+      // setUrl(stream);
+      source.start();
+    });
+  };
+
+  const handleFiles: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const files = event.currentTarget.files;
+    if (!files || files?.length === 0) return;
+    const file = files[0];
+    // console.log("file:", file);
+    setFile(file.name);
+    setUrl(URL.createObjectURL(file));
+  };
 
   return (
     <Layout title="Home | splish (speak and listen, shadow)">
@@ -64,6 +98,13 @@ const IndexPage = () => {
             {soundButtonLabel}
           </button>
         </div>
+
+        <div>テスト: {file}</div>
+        <button onClick={onClickTest}>テスト</button>
+        <input type="file" onChange={handleFiles} />
+        {/* <div>url: {url}</div> */}
+        {/* <ReactPlayer url={url} controls={true} height="100px"></ReactPlayer> */}
+        <audio controls src={url}></audio>
       </div>
     </Layout>
   );
